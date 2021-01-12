4003bb8831b109f500fa3430c42243e4629b2390
Mambisi Zempare <lilbizi96@gmail.com>
2021-01-12 19:45:56 +0000

    Testing completed

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -3,13 +3,3 @@ mod file;
 
 pub use crate::file::ActionsFileReader;
 pub use crate::file::ActionsFileWriter;
-
-#[cfg(test)]
-mod tests {
-    use crate::ActionsFileReader;
-
-    #[test]
-    fn it_works() {
-
-    }
-}
