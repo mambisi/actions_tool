5ceec8ce1abfd3e5dd6e1aec6104f35d879bff0e
Mambisi Zempare <lilbizi96@gmail.com>
2021-01-13 18:58:34 +0000

    compression added

--- bintool/src/main.rs
+++ bintool/src/main.rs
@@ -4,13 +4,17 @@ use std::time::Instant;
 use jemalloc_ctl::{stats, epoch};
 use io::channel::{Block, ContextAction};
 
-use merkle::prelude::{MerkleStorageStats,MerklePerfStats,MerkleError};
+use merkle::prelude::{MerkleStorageStats, MerklePerfStats, MerkleError};
 
 use std::sync::{RwLock, Arc};
 use std::convert::TryInto;
+use flate2::read::DeflateDecoder;
+use flate2::write::DeflateEncoder;
+use flate2::Compression;
 
 #[cfg(not(target_env = "msvc"))]
 use jemallocator::Jemalloc;
+use std::fs::{File, OpenOptions, read};
 
 #[cfg(not(target_env = "msvc"))]
 #[global_allocator]
@@ -58,6 +62,38 @@ fn main() {
             .takes_value(true)
         )
     )
+        .subcommand(App::new("compress")
+            .about("Compress bin file with snap-rs")
+            .arg(Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("FILE NAME")
+                .help("input file")
+                .takes_value(true)
+            ).arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("FILE NAME")
+            .help("output file")
+            .takes_value(true)
+        )
+        )
+        .subcommand(App::new("uncompress")
+            .about("Compress bin file with snap-rs")
+            .arg(Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("FILE NAME")
+                .help("input file")
+                .takes_value(true)
+            ).arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("FILE NAME")
+            .help("output file")
+            .takes_value(true)
+        )
+        )
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("print") {
@@ -68,7 +104,7 @@ fn main() {
 
         if let Some(file) = matches.value_of("block") {
             let reader = ActionsFileReader::new(file).unwrap();
-            reader.for_each(|(block,_)|{
+            reader.for_each(|(block, _)| {
                 println!("[{:<10}] {}", block.block_level, block.block_hash)
             })
         }
@@ -98,7 +134,7 @@ fn main() {
         let file = matches.value_of("file").unwrap();
         let mut reader = ActionsFileReader::new(file).unwrap();
         // Todo make cycle user defined
-        let stats = match validate_blocks_merkle_gc_enabled(reader,4092) {
+        let stats = match validate_blocks_merkle_gc_enabled(reader, 4092) {
             Ok(stats) => {
                 stats
             }
@@ -109,15 +145,37 @@ fn main() {
         print_stats(stats);
         return;
     }
+    if let Some(matches) = matches.subcommand_matches("compress") {
+        let input_path = matches.value_of("input").unwrap();
+        let output_path = matches.value_of("output").unwrap();
+
+        let mut input_file = OpenOptions::new().read(true).write(false).create(false).open(input_path).expect("Error opening input file");
+        let mut output_file = OpenOptions::new().read(true).write(true).create(true).open(output_path).unwrap();
+
+        let mut writer = DeflateEncoder::new(output_file,Compression::best());
+        std::io::copy(&mut input_file, &mut writer).expect("Error coping file");
+        return;
+    }
+    if let Some(matches) = matches.subcommand_matches("uncompress") {
+        let input_path = matches.value_of("input").unwrap();
+        let output_path = matches.value_of("output").unwrap();
+
+        let input_file = OpenOptions::new().read(true).write(false).create(false).open(input_path).expect("Error opening input file");
+        let mut output_file = OpenOptions::new().read(true).write(true).create(true).open(output_path).unwrap();
+
+        let mut reader = DeflateDecoder::new(input_file);
+        std::io::copy(&mut reader, &mut output_file).expect("Error coping file");
+        return;
+    }
 }
 
 
-fn validate_blocks_merkle_gc_enabled(reader : ActionsFileReader, cycle : u32) -> Result<MerkleStorageStats,MerkleError> {
+fn validate_blocks_merkle_gc_enabled(reader: ActionsFileReader, cycle: u32) -> Result<MerkleStorageStats, MerkleError> {
     use merkle::prelude::*;
     let db = Arc::new(RwLock::new(DB::new()));
 
     let mut storage = MerkleStorage::new(db.clone());
-    reader.for_each(|(block,actions)|{
+    reader.for_each(|(block, actions)| {
         let block_level = block.block_level;
         for action in &actions {
             match action {
@@ -160,7 +218,6 @@ fn validate_blocks_merkle_gc_enabled(reader : ActionsFileReader, cycle : u32) ->
                 }
                 _ => (),
             };
-
         }
         if block_level != 0 && block_level % cycle == 0 {
             storage.gc();
@@ -187,12 +244,11 @@ fn print_stats(stats: MerkleStorageStats) {
     println!("--------------------------------------------------------------------------------------");
     for (k, v) in stats.perf_stats.perpath.iter() {
         println!("{}", k);
-        for (k,v) in v {
+        for (k, v) in v {
             println!("          {:<35}{}", format!("{} AVG EXEC TIME:", k.to_uppercase()), v.avg_exec_time);
             println!("          {:<35}{}", format!("{} OP EXEC TIME MAX:", k.to_uppercase()), v.op_exec_time_max);
             println!("          {:<35}{}", format!("{} OP EXEC TIME MIN:", k.to_uppercase()), v.op_exec_time_min);
             println!("          {:<35}{}", format!("{} OP EXEC TIMES:", k.to_uppercase()), v.op_exec_times);
         }
     }
-
 }
